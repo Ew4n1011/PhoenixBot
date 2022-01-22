c67c7b509f55a76a0879a3b9013d876f320dc08d
Ewan Lee <ewanlee1011@gmail.com>
2022-01-22 16:56:31 +0100

    AFK Multi Guild

---
+++ Events/Message/AFKSystem.js
@@ -0,0 +1,26 @@
+const {Message, MessageEmbed} = require('discord.js')
+const DB = require('../../Structures/Schemas/AFKSystem')
+
+module.exports = {
+    name: "messageCreate",
+    /**
+     * 
+     * @param {Message} message 
+     */
+    async execute(message) {
+        if(message.author.bot) return
+
+        if(message.mentions.members.size) {
+            const Embed = new MessageEmbed()
+            .setColor("RED")
+            message.mentions.members.forEach((m) => {
+                DB.findOne({GuildID: message.guild.id, UserID: m.id}, async (err, data) => {
+                    if(err) throw err
+                    if(data)
+                    Embed.setDescription(`${m} went AFK <t:${data.Time}:R>\n **Status**: ${data.Status}`)
+                    return message.reply({embeds: [Embed]})
+                })
+            })
+        }
+    }
+}
